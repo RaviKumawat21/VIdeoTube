368df7e55998c15f5c731011ebc62ba8753d5228
RaviKumawat21 <ravikumawat8860@gmail.com>
2025-08-23 15:21:14 +0530

    add refreshAccessToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 // We are using curly braces because expree uses the name export of the Router
 import { Router } from "express"; 
-import {registerUser, loginUser, logoutUser} from "../controllers/user.controller.js"
+import {registerUser, loginUser, logoutUser,refreshAccessToken} from "../controllers/user.controller.js"
 import upload from "../middlewares/multer.middleware.js"
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -21,5 +21,6 @@ router.route("/register").post(
     ,registerUser);
 
 router.route("/login").post(loginUser) ;
-router.route("/logout").post(varifyJWT,logoutUser);   
+router.route("/logout").post(varifyJWT,logoutUser); 
+router.route("/refresh-token").post(refreshAccessToken);  
 export default router;
